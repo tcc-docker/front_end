6bbefc8a453e52379c950fe7a1aa75f6ff52842c
Amanda Oliveira <amd.gaoliveira@gmail.com>
2023-08-28 22:11:20 -0300

    runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  $production: {
+    routeRules: {
+      '/**': { isr: true }
+    }
+  },
+  $development: {
+    //
+  },
+  $meta: {
+    //
+  },
+  devtools: { enabled: true },
+  runtimeConfig: {
+    // The private keys which are only available server-side
+    apiSecret: process.env.NUXT_API_SECRET,
+    // Public keys that are exposed to the client
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || '/api'
+    }
+  }
 })
