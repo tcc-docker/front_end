3194133dfd0344e4183ae76de973d74652fc3d43
Amanda Oliveira <amd.gaoliveira@gmail.com>
2023-08-30 17:07:59 -0300

    google-fonts config module

--- assets/README.md
+++
@@ -1,5 +0,0 @@
-# ASSETS
-
-**Este diretório não é Obrigatório, você pode deletá-lo se não for utilizar.**
-
-Contém aquivos de script e estilização, como também pré-processadores.
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  // CONFIGURAÇÃO DEVTOOLS
   devtools: { enabled: true },
+
+  // VARIÁVEIS DE PRODUÇÃO
   $production: {
     routeRules: {
       '/**': { isr: true }
     }
   },
+
+  // VARIÁVEIS DE DESENVOLVIMENTO
   $development: {
     //
   },
+
+  // CONFIGURAÇÕES DE TEMPO DE EXECUÇÃO
   runtimeConfig: {
     // The private keys which are only available server-side
     apiSecret: process.env.NUXT_API_SECRET,
@@ -17,14 +24,23 @@ export default defineNuxtConfig({
       apiBase: process.env.NUXT_PUBLIC_API_BASE || '/api'
     }
   },
+
+  // CONFIGURAÇÕES DE SEO
   app: {
     head: {
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: "charset", content: "utf-8" }
+        { name: 'charset', content: 'utf-8' },
       ],
+      link: [
+        { rel: 'dns-prefetch', href: 'https://fonts.gstatic.com/' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com/', crossorigin: '' },
+        { rel: 'preload', as: 'style', href: 'https://fonts.googleapis.com/css2?family=Roboto' }
+      ]
     }
   },
+
+  // CONFIGURAÇÃO DE COMPONENTES
   components: {
     global: true,
     dirs: [
@@ -32,4 +48,29 @@ export default defineNuxtConfig({
       { path: '~/components/compositions', pathPrefix: false }
     ]
   },
+
+  // MODULOS
+  modules: [
+    ['@nuxtjs/google-fonts']
+  ],
+
+  // GOOGLE FONTS
+  googleFonts: {
+    prefetch: true,
+    preconnect: true,
+    preload: true,
+    download: true, // Permite baixar css e fonts do Google para o Projeto.
+    overwriting: true, // Impede que os arquivos sejam baixados mais de uma vez.
+    fontsDir: 'fonts', // Diretório que as fontes serão baixadas.
+    fontsPath: 'fonts', // Caminho das fontes.
+    outputDir: 'assets', // Diretório de saída para arquivos baixados.
+    stylePath: 'google-fonts.css', // Caminho da folha de estilo gerada.
+    // Display lets you control what happens while the font is still loading or otherwise unavailable.
+    display: 'auto', // 'auto' | 'block' | 'swap' | 'fallback' | 'optional'
+    families: {
+      Roboto: true,
+      Inter: [400, 700]
+    }
+  }
+
 })
