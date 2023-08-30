6105b502d71bb44a723a9e5b7f022e8ac59a9fdf
Amanda Oliveira <amd.gaoliveira@gmail.com>
2023-08-30 16:24:07 -0300

    configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  devtools: { enabled: true },
   $production: {
     routeRules: {
       '/**': { isr: true }
@@ -8,10 +9,6 @@ export default defineNuxtConfig({
   $development: {
     //
   },
-  $meta: {
-    //
-  },
-  devtools: { enabled: true },
   runtimeConfig: {
     // The private keys which are only available server-side
     apiSecret: process.env.NUXT_API_SECRET,
@@ -19,5 +16,20 @@ export default defineNuxtConfig({
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE || '/api'
     }
-  }
+  },
+  app: {
+    head: {
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: "charset", content: "utf-8" }
+      ],
+    }
+  },
+  components: {
+    global: true,
+    dirs: [
+      '~/components',
+      { path: '~/components/compositions', pathPrefix: false }
+    ]
+  },
 })
